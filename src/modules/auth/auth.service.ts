45e62a6a026ff1e01b10d83eaf9f3364720586ce
Alan de Jesus Soto Flores <alansoto@MBA20-Alan-Soto.local>
2024-12-19 22:38:41 -0600

    fixed auth service

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -25,17 +25,18 @@ export class AuthService {
   async validateUser(email: string, password: string): Promise<any> {
     const user = await this.userService.findOneByEmail(email);
 
+    if (!user) return null;
+
     const isMatch = await bcrypt.compare(password, user.password);
 
-    if (user && isMatch) {
-      return {
-        id: user.id,
-        email: user.email,
-        firstName: user.firstName,
-        lastName: user.lastName,
-      };
-    }
-    return null;
+    if (!user || !isMatch) return null;
+
+    return {
+      id: user.id,
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    };
   }
 
   async login(user: any): Promise<{
